85235e698e83af1f741893f841e983d8abe2b9c6
Mike Zhao <mike.zhao@intelerad.com>
2020-11-06 19:17:32 -0500

    fix script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 docker build -t yong9900/multi-client:latest -t yong9900/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t yong9900/multi-server:latest -t yong9900/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t yong9900/multi-worker:latest -t yong9900/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push -t yong9900/multi-client:latest
-docker push -t yong9900/multi-client:$SHA
-docker push -t yong9900/multi-server:latest
-docker push -t yong9900/multi-server:$SHA
-docker push -t yong9900/multi-worker:latest
-docker push -t yong9900/multi-worker:$SHA
+
+docker push yong9900/multi-client:latest
+docker push yong9900/multi-server:latest
+docker push yong9900/multi-worker:latest
+
+docker push yong9900/multi-client:$SHA
+docker push yong9900/multi-server:$SHA
+docker push yong9900/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=yong9900/multi-server:$SHA
